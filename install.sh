98c3d8d92ca128fce22f3cf703212bf51d3efff1
Ryan Martin <54093748+eqpoqpe@users.noreply.github.com>
2022-01-24 15:20:47 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,4 @@
 # #!/bin/bash
 
-declare readonly DEFAULT_INSTALL_DIR="/usr/local/lib/blcc/"
-declare readonly BLCC_DIR_R="blcc"
-declare readonly UTIL_DIR_R="${BLCC_DIR_R}_util"
-
-declare readonly SCRIPT_DIR=("./script/blcc.sh" "./script/util.sh")
-
-for gs in ${SCRIPT_DIR[@]}; do
-	echo $gs
-done
+declare readonly RCONF_URL="https://github.com/eqpoqpe/rconf.git"
+declare readonly RANDOM_URL="https://github.com/eqpoqpe/spiralc.git"
